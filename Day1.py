53fff71a662caea5fed8c162b5bc5e6be75ea118
Jose Sejas <escobarjose516@gmail.com>
2025-08-27 03:36:20 -0400

    first day python self learn

---
+++ Day1.py
@@ -0,0 +1,17 @@
+#Print() function to display on screen for example strongs or run a function 
+#print("Hey Pal!")
+
+#input() function to get users input 
+
+#name = input("What is your name")
+
+#print(name)
+
+print("hey Pal! Ill give you a bandname recommendation")
+
+firstQ = input("where were you born? ")
+print(firstQ)
+secondQ = input("what is the name of your pet? ")
+print(secondQ)
+
+print("the name of the band is " + secondQ + " " + firstQ)
